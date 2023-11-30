547a2e2a3f6ed80440663771a7adf50f6f315c42
Faisal Ahmed Shourab <86229814+shourab-dev@users.noreply.github.com>
2023-11-30 17:47:13 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+
+//* rules of variable
+
+$name = "Harvey";
+
+
+
+
+
+$age = 23;
+
+$hobby = "football";
+
+// echo "I am $name. I am $age years old. I love playing $hobby.";
+
+
+/**
+ * * VALUE VARY 
+ */
+
+
+define('PI', 3.1416);
+
+echo "PI";
